c366138c021a952b64d3c132ffab182f79aa078b
Phoenix <x.rhythm@qq.com>
2017-03-21 17:52:24 +0800

    clear tableviewcell

---
+++ ClearTableViewCell/ClearTableViewCell/GradientCell.swift
@@ -0,0 +1,49 @@
+//
+//  GradientCell.swift
+//  ClearTableViewCell
+//
+//  Created by Phoenix on 2017/3/21.
+//  Copyright © 2017年 Wolkamo. All rights reserved.
+//
+
+import UIKit
+
+class GradientCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let color1 = UIColor(white: 1.0, alpha: 0.2).cgColor
+        let color2 = UIColor(white: 1.0, alpha: 0.1).cgColor
+        let color3 = UIColor.clear.cgColor
+        let color4 = UIColor(white: 0, alpha: 0.05).cgColor
+        
+        gradientLayer.colors = [color1, color2, color3, color4]
+        gradientLayer.locations = [0.0, 0.04, 0.95, 1.0]
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.gradientLayer.frame = self.bounds
+    }
+    
+
+}
---
+++ ClearTableViewCell/ClearTableViewCell/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ClearTableViewCell
+//
+//  Created by Phoenix on 2017/3/21.
+//  Copyright © 2017年 Wolkamo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    var tableData = ["Read 3 article on Medium", "Cleanup bedroom", "Go for a run", "Hit the gym", "Build another swift project", "Movement training", "Fix the layout problem of a client project", "Write the experience of #30daysSwift", "Inbox Zero", "Booking the ticket to Chengdu", "Test the Adobe Project Comet", "Hop on a call to mom"]
+    
+    var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
+        self.tableView.frame = self.view.bounds
+        self.tableView.dataSource = self
+        self.tableView.rowHeight = 60
+        self.tableView.backgroundColor = UIColor.black
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        self.tableView.register(GradientCell.self, forCellReuseIdentifier: "tableviewcell")
+        view.addSubview(self.tableView)
+      
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableviewcell", for: indexPath)
+        cell.textLabel?.text = tableData[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.backgroundColor = UIColor.clear
+        cell.textLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        cell.backgroundColor = colorForIndex(indexPath.row)
+        return cell
+    }
+    
+    func colorForIndex(_ index: Int) -> UIColor {
+        let itemCount = tableData.count - 1
+        let color = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: 1.0, green: color, blue: 0.0, alpha: 1.0)
+    }
+
+}
+
